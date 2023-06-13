5a7a231f2b9221ae234d4b81708c6f6e2cae398e
toswe <toswesweswe@gmail.com>
2023-06-13 14:10:43 +0200

    Extract simplex processing to separate functions

--- process_history.py
+++ process_history.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plot
 import gudhi
 import numpy
 
+FIRE_TILE_TYPE = 2
 TRIVIAL_HOLES = {
     (3, (1.4142135623730951, 1.7320508075688772)),
     (1, (1.0, 1.4142135623730951)),
@@ -14,8 +15,9 @@ TRIVIAL_HOLES = {
     (1, (0.25, 0.5)),
     (0, (0.0, numpy.inf)),
 }
+
 USE_ALPHA_COMPLEX = True
-FIRE_TILE_TYPE = 2
+MAX_INTERVALS_BARCODE_CHART = 30  # set to 0 to show all
 
 
 def history_to_points(history):
@@ -31,43 +33,56 @@ def history_to_points(history):
     return points
 
 
-def process_history(history):
-    print(f"History length: {len(history)}")
-    points = history_to_points(history)
+def process_points_alpha(points):
+    print("Creating Alpha Complex")
+    alpha = gudhi.AlphaComplex(points=points)
 
+    print("Creating simplex tree")
+    simplex_tree = alpha.create_simplex_tree()
+
+    print("Running simplex_tree.persistence()")
+    return simplex_tree.persistence()
+
+
+# Deprecated, too slow and memory intensive
+def process_points_rips(points):
+    # rips = gudhi.RipsComplex(points=points, max_edge_length=2.1, sparse=0.25)
+    rips = gudhi.RipsComplex(points=points, max_edge_length=2.5)
+
+    print("Creating simplex tree")
+    simplex_tree = rips.create_simplex_tree(max_dimension=3)
+
+    print("Running simplex_tree.persistence()")
+    return simplex_tree.persistence(homology_coeff_field=2, min_persistence=0)
+
+
+def process_points(points):
     if USE_ALPHA_COMPLEX:
-        print("Creating Alpha Complex")
-        alpha = gudhi.AlphaComplex(points=points)
-
-        print("Creating simplex tree")
-        simplex_tree = alpha.create_simplex_tree()
-
-        print("Running simplex_tree.persistence()")
-        diag = simplex_tree.persistence()
-        diag = [d for d in diag if d not in TRIVIAL_HOLES]
-
+        diag = process_points_alpha(points)
     else:
         # Deprecated, too slow and memory intensive
-
         print("Creating Rips Complex")
-        # rips = gudhi.RipsComplex(points=points, max_edge_length=2.1, sparse=0.25)
-        rips = gudhi.RipsComplex(points=points, max_edge_length=2.5)
-
-        print("Creating simplex tree")
-        simplex_tree = rips.create_simplex_tree(max_dimension=3)
-
-        print("Running simplex_tree.persistence()")
-        diag = simplex_tree.persistence(homology_coeff_field=2, min_persistence=0)
+        diag = process_points_rips(points)
 
     diag = [d for d in diag if d not in TRIVIAL_HOLES]
     # print("diag=", diag)
 
     gudhi.plot_persistence_diagram(diag)
-    gudhi.plot_persistence_barcode(diag, max_intervals=30)
+    gudhi.plot_persistence_barcode(diag, max_intervals=MAX_INTERVALS_BARCODE_CHART)
 
+
+def render_3D_fire(history):
     print("Rendering 3D plot for fire tiles")
     ax = plot.figure().add_subplot(projection="3d")
     ax.voxels(numpy.array(history) == FIRE_TILE_TYPE, edgecolor="k")
+
+
+def process_history(history):
+    print(f"History length: {len(history)}")
+    points = history_to_points(history)
+
+    process_points(points)
+    render_3D_fire(history)
 
     plot.show()
 
